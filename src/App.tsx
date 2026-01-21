6e38e0cec683965fff9bcd7b5934752a15b971dd
Nicolas Donada <nicolasdonada4@gmail.com>
2026-01-21 16:26:48 -0300

    feat: adicionado funcoes de listar, deletar e editar cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home";
-import Cadastro from "./pages/cadastro/Cadastro";
-import Login from "./pages/login/Login";
-import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
+import FormTema from './components/tema/formtema/FormTema'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
+import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <>
-    <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,41 +1,36 @@
-import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-    const navigate = useNavigate()
-    const {usuario,handleLogout} = useContext(AuthContext)
 
-    function sair(){
-        handleLogout()
-        navigate('/')
-    }
+  const navigate = useNavigate();
 
+  const { handleLogout } = useContext(AuthContext)
 
-    return (
-        <>
-            <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
-            
-                <div className="container flex justify-between text-lg mx-8">
-                    <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
-                    <img src={usuario.foto} alt="" width={'60px'}/>
-                     <p>Bem vindo!</p>
-                    <p>{usuario.nome}</p> 
-                   
-                    
-                    <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        <Link to='' onClick={sair} className="hover:underline"> Sair </Link>
-                                             
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  function logout(){
+    handleLogout()
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
+
+  return (
+    <>
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
+
+          <div className='flex gap-4'>
+            Postagens
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+            Perfil
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Navbar
